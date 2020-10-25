b0d38be5b6971485da8da766c6889609765e4f28
panda-dev <nada830120@gmail.com>
2020-10-25 17:46:08 +0000

    Completed chatting room

--- App.js
+++ App.js
@@ -10,8 +10,9 @@ import React from 'react';
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux'
-import { View, Image } from 'react-native';
+import { View, Image,Text } from 'react-native';
 import reducer from './src/reducers';
+import images from './src/config/images';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
@@ -32,6 +33,74 @@ const middleware = [thunk];
 const composeEnhancer = global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 const enhancer = composeEnhancer(applyMiddleware(...middleware));
 const store = createStore(reducer, {}, enhancer);
+function MyTabs() {
+  return (
+    <Tab.Navigator
+      initialRouteName="Chat"
+      tabBarOptions={{
+        style: {
+          backgroundColor: '#f7f7f7',
+          //----------add this line------------------------//
+          height: 60,
+          borderTopWidth: 1,
+        },
+      }}
+    >
+      <Tab.Screen
+        name="Chat"
+        component={Dashboard}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <View>
+            <Image source={images.Chat} style={{width: 18, height: 18, tintColor: color, marginTop:20 }}/>
+            <Text style={{ fontSize:10}}>Chat</Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="profile"
+        component={Dashboard}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <View>
+            <Image source={images.Profile} style={{width: 16, height: 18, tintColor: color, marginTop:20, marginLeft:10 }}/>
+            <Text style={{ fontSize:10}}>My profile</Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Setting"
+        component={Dashboard}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <View>
+            <Image source={images.Setting} style={{width: 22, height: 18, tintColor: color, marginTop:20, marginLeft:3 }}/>
+            <Text style={{ fontSize:10}}>Setting</Text>
+            </View>
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Dolphies"
+        component={Dashboard}
+        options={{
+          tabBarLabel: '',
+          tabBarIcon: ({ color, size }) => (
+            <View>
+            <Image source={images.Dolphy} style={{width: 22, height: 18, tintColor: color, marginTop:20, marginLeft:5 }}/>
+            <Text style={{ fontSize:10}}>Dolphies</Text>
+            </View>
+          ),
+        }}
+      />
+    </Tab.Navigator>
+  );
+}
 
 function Navigator() {
   return (
@@ -39,7 +108,7 @@ function Navigator() {
       <Stack.Navigator screenOptions={{ cardStyle: { backgroundColor: '#fff' } }} initialRouteName='Onboarding'>
         <Stack.Screen name="Onboarding" component={Onboarding} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
-        <Stack.Screen name="Dashboard" component={Dashboard} options={{ headerShown: false }} />
+        <Stack.Screen name="Dashboard" component={MyTabs} options={{ headerShown: false, gestureEnabled: false }} />
         <Stack.Screen name="Vendertwosignup" component={Vendertwosignup} options={{ headerShown: false }} />
         <Stack.Screen name="Clienttwosignup" component={Clienttwosignup} options={{ headerShown: false }} />
         <Stack.Screen name="Venderthreesignup" component={Venderthreesignup} options={{ headerShown: false }} />
--- src/config/images.js
+++ src/config/images.js
@@ -1,6 +1,9 @@
 const images = {
     CUT: require('../assets/images/CUT.png'),
-   
+    Chat: require('../assets/images/chat.png'),
+    Profile: require('../assets/images/profile.png'),
+    Setting: require('../assets/images/setting.png'),
+    Dolphy: require('../assets/images/dolphy.png'),
 }
 
 export default images;
